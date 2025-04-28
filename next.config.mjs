c9ac5f91b1a02c87665a93e058e9e75614b8e147
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-04-28 12:51:29 -0300

    Se aplico los cambion en la api para verificar si funciona el estatico en el cpanel

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         URL_PDF: "https://www.tucumanturismo.gob.ar/public/files/",
         URL_IMG: "https://www.tucumanturismo.gob.ar/public/img/",
-        URL_SERVER: 'http://10.20.20.5/promocional25/api/'
+        URL_SERVER: 'https://www.tucumanturismo.gob.ar/api/'
     },
     output: 'export',
     images: {
