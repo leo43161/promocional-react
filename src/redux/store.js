e84fe0a08bfacccfb0d12a8c6e697c577a9e13c3
unknown <natim02@gmail.com>
2025-06-30 09:04:05 -0300

    subo lo que falta

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
-/* import orderReducer from './slices/orderSlice';
-import cartReducer from './slices/cartSlice';
-import productReducer from './slices/productSlice';
-import categoryReducer from './slices/categorySlice';
-import uiReducer from './slices/uiSlice'; */
-import { eventosService } from './services/eventosService';
 import { prestadoresService } from './services/prestadoresService';
 import { articulosService } from './services/articulosService';
 import { alojamientosService } from './services/alojamientosService';
+import { eventosService } from './services/eventosService';
+import { headerService } from './services/headerService';
+import { blogService } from './services/blogService';
+import { busquedaArtService } from './services/busquedaArtService';
 
 export const store = configureStore({
   reducer: {
-    /* orders: orderReducer,
-    cart: cartReducer,
-    products: productReducer,
-    categories: categoryReducer,
-    ui: uiReducer, */
     [eventosService.reducerPath]: eventosService.reducer,
     [prestadoresService.reducerPath]: prestadoresService.reducer,
     [articulosService.reducerPath]: articulosService.reducer,
     [alojamientosService.reducerPath]: alojamientosService.reducer,
+    [headerService.reducerPath]: headerService.reducer,
+    [blogService.reducerPath]: blogService.reducer,
+    [busquedaArtService.reducerPath]: busquedaArtService.reducer
+
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       eventosService.middleware,
       prestadoresService.middleware,
       alojamientosService.middleware,
+      articulosService.middleware,
+      headerService.middleware,
+      blogService.middleware,
+      busquedaArtService.middleware,
     ),
 });
 
