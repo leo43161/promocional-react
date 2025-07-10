34f2b1a59f71392a98f043870e5ead5ebc840812
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-07-10 14:58:28 -0300

    Se esta probando zipear el proyecto despues pasarlo por ftp

--- src/pages/itinerario.jsx
+++ src/pages/itinerario.jsx
@@ -95,7 +95,7 @@ export default function Itinerario() {
 
     return (
       <div className="h-screen w-full bg-background">
-        {window.innerWidth < 1024 ? (
+        {true ? (
           // --- VISTA PARA MÓVIL ---
           <div className="flex flex-col justify-center items-center h-full text-center p-8">
             <svg className="w-24 h-24 text-secondary mb-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
@@ -125,7 +125,6 @@ export default function Itinerario() {
             </PDFDownloadLink>
           </div>
         ) : (
-          // --- VISTA PARA ESCRITORIO ---
           <ItinerarioViewer data={dataArray} />
         )}
       </div>
