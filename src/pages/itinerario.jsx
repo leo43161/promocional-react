d121e4a9df7e15661d69cede8a7d358d837e759a
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-09-23 15:46:23 -0300

    Se tradujo el creador de itinerario

--- src/pages/itinerario.jsx
+++ src/pages/itinerario.jsx
@@ -17,6 +17,7 @@ export default function Itinerario() {
   const router = useRouter();
   const { id } = router.query;
   const language = getCurrentLanguage(router.query);
+  console.log(language);
   const isSpanish = language.code === 'ES';
 
   const ItinerarioViewer = dynamic(
