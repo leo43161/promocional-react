499e4212c33175a58603427b5448df03f97bdb0f
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2025-10-28 11:03:20 -0300

    Se agrego una probabilidad de aparicion de wayki del 40% y que aparezca cada 2 min

--- src/components/wayki/FunFacs.jsx
+++ src/components/wayki/FunFacs.jsx
@@ -4,43 +4,46 @@ import { motion, AnimatePresence, animate } from 'framer-motion';
 import { X } from 'lucide-react';
 import Image from 'next/image';
 
-// (Asumimos que CURIOSIDADES sigue siendo el mismo array)
-const CURIOSIDADES = [
-    {
-        text: '¿Sabías que Tucumán es la provincia más pequeña de Argentina?',
-        link: '/articulos/tucuman-pequena',
-        imageSrc: 'https://www.tucumanturismo.gob.ar/public/img/casahistorica_nrxunv4s_16-07-2025.jpg' // <-- Ruta de ejemplo
-    },
-    {
-        text: 'La Casa Histórica fue declarada Monumento Histórico Nacional en 1941.',
-        link: '/articulos/casa-historica',
-        imageSrc: 'https://www.tucumanturismo.gob.ar/public/img/casahistorica_nrxunv4s_16-07-2025.jpg' // <-- Ruta de ejemplo
-    },
-    {
-        text: 'Tucumán es conocido como el "Jardín de la República".',
-        link: '/articulos/jardin-republica',
-        imageSrc: 'https://www.tucumanturismo.gob.ar/public/img/casahistorica_nrxunv4s_16-07-2025.jpg' // <-- Este no tendrá imagen
-    }
-];
 export default function FunFacs() {
     const [isVisible, setIsVisible] = useState(false);
-    const [currentFact, setCurrentFact] = useState(CURIOSIDADES[0]);
+    const [currentFact, setCurrentFact] = useState(null);
 
     // Lógica para mostrarlo/ocultarlo (igual que antes)
     useEffect(() => {
-        const showInterval = setInterval(() => {
-            // Elegir un fact al azar
-            const randomFact = CURIOSIDADES[Math.floor(Math.random() * CURIOSIDADES.length)];
-            setCurrentFact(randomFact);
-            setIsVisible(true);
-
+        if (Math.random() < 0.4) {
             setTimeout(() => {
-                setIsVisible(false);
+                handleFunFact();
             }, 4000);
-        }, 5000);
+        }
+        const showInterval = setInterval(async () => {
+            try {
+                handleFunFact();
+            } catch (error) {
+                setIsVisible(false);
+            }
+        }, 120000);
 
         return () => clearInterval(showInterval);
     }, []);
+
+    const handleFunFact = async () => {
+        try {
+            const url = 'https://tucumanturismo.gob.ar/api/v1/api/wayki';
+            const response = await fetch(url);
+            if (!response.ok) {
+                setIsVisible(false);
+                throw new Error("WARN", response.status);
+            }
+            const data = await response.json();
+            setCurrentFact(data.result[0]);
+            setIsVisible(true);
+            setTimeout(() => {
+                setIsVisible(false);
+            }, 15000);
+        } catch (error) {
+            setIsVisible(false);
+        }
+    }
 
     const waykiVariants = {
         hidden: {
@@ -61,10 +64,10 @@ export default function FunFacs() {
             }
         },
         exit: {
-            opacity: 0,
-            x: [-4, 100],
-            y: [-4, 100],
-            filter: ['blur(0px)', 'blur(1px)', 'blur(3px)'],
+            opacity: [0.8, 0.4, 0],
+            x: [-6, 100],
+            y: [-6, 100],
+            filter: ['blur(0.4px)', 'blur(2px)', 'blur(3px)'],
             transition: {
                 duration: .4,
                 ease: "easeInOut"
@@ -96,7 +99,7 @@ export default function FunFacs() {
 
     return (
         <AnimatePresence>
-            {isVisible && (
+            {isVisible && currentFact && (
                 <motion.div
                     className="fixed z-50 flex items-end -right-24 md:bottom-5 bottom-5 md:gap-9 gap-8"
                     // Animación general del contenedor (aparecer/desaparecer)
@@ -111,31 +114,33 @@ export default function FunFacs() {
                     >
                         <button
                             onClick={() => setIsVisible(false)}
-                            className="absolute top-3 right-3 z-10 rounded-full p-1 text-gray-400 bg-white transition-all hover:bg-gray-200 hover:text-gray-400 hover:scale-110 drop-shadow-2xl"
+                            className="absolute top-3 right-3 z-[5] rounded-full p-1 text-gray-400 bg-white transition-all hover:bg-gray-200 hover:text-gray-400 hover:scale-110 drop-shadow-2xl"
                             aria-label="Cerrar"
                         >
                             <X size={18} />
                         </button>
-                        {currentFact.imageSrc && (
-                            <div className="relative md:h-35 h-30 w-full">
-                                <Image
-                                    src={currentFact.imageSrc}
-                                    alt={currentFact.text}
-                                    fill
-                                    style={{ objectFit: 'cover' }} 
-                                    sizes="(max-width: 768px) 100vw, 250px" 
-                                />
+                        <a href={process.env.URL_LOCAL + currentFact.link}>
+                            {currentFact.img && (
+                                <div className="relative md:h-35 h-30 w-full">
+                                    <Image
+                                        src={currentFact.img}
+                                        alt={currentFact.texto}
+                                        fill
+                                        style={{ objectFit: 'cover' }}
+                                        sizes="(max-width: 768px) 100vw, 250px"
+                                    />
+                                </div>
+                            )}
+                            <div className="px-4 py-3.5 bg-secondary">
+                                <p className="mb-3 md:text-3xl xl:text-4xl text-xl font-bold px-1">{currentFact.texto}</p>
+                                <a
+                                    href={process.env.URL_LOCAL + currentFact.link}
+                                    className="inline-block rounded-md bg-primary px-3 py-1 text-sm md:text-md font-bold text-white transition-transform hover:scale-105"
+                                >
+                                    Leer más
+                                </a>
                             </div>
-                        )}
-                        <div className="px-4 py-3.5 bg-secondary">
-                            <p className="mb-3 md:text-3xl xl:text-4xl text-xl font-bold px-1">{currentFact.text}</p>
-                            <a
-                                href={currentFact.link}
-                                className="inline-block rounded-md bg-primary px-3 py-1 text-sm md:text-md font-bold text-white transition-transform hover:scale-105"
-                            >
-                                Leer más
-                            </a>
-                        </div>
+                        </a>
                     </motion.div>
 
                     {/* --- 1. Wayki (El Tapir) --- */}
